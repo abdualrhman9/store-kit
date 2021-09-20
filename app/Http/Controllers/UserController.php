3cb901774eab311738bf6e35008bed12d71f1330
unknown <abdoalrhman9@hotmail.com>
2021-09-20 12:46:02 -0700

    fix password validation error

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
         $data = $request->validate([
             'name'=>'required',
             'email'=>'required|email|string|max:255|unique:users',
-            'password'=>'required|string|min:8|confirmed',
+            'password'=>'required|string|min:6|confirmed',
         ]);
 
         $user = User::create([
